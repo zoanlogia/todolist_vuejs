cc7943d9987d502b3686084cd7d0440289e2153e
guillaume <g.flambard@gmail.com>
2021-07-29 17:06:59 +0200

    problème "todos is not a function..."

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <todos></todos>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  components: {
+    Todos
+  }
+}
+</script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Todos</h1>
+      <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo" @keyup.enter="addToDo">
+    </header>
+    <div class="main">
+      <ul class="todo-list">
+        <li class="todo" v-for="todo in filteredTodos" :class="{completed: todo.completed}">
+          <div class="view">
+            <input type="checkbox" v-model="todo.completed" class="toggle">
+            <label>{{ todo.name }}</label>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <footer class="footer">
+      <span class="todo-count"><strong>{{ remaining }}</strong> tâches à faire</span>
+      <ul class="filters">
+        <li><a href="#" class="{selected: filter === 'all}" @click.prevent="filter = 'all'">Toutes</a></li>
+        <li><a href="#" class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire</a></li>
+        <li><a href="#" class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
+      </ul>
+    </footer>
+  </section>
+</template>
+
+<script>
+
+import todo from './todo'
+export default {
+  data () {
+    return {
+      todos: [
+        {
+          name: 'Tâche de test',
+          completed: false
+        }
+      ],
+      newTodo: '',
+      filter: 'all'
+    }
+  },
+  methods: {
+    addToDo () {
+      this.todos.push({
+        completed: false,
+        name: this.newTodo
+      })
+      this.newTodo = ''
+    }
+  },
+  computed: {
+    remaining () {
+      return this.todos.filter(todo => !todo.completed).length
+    },
+    filteredTodos () {
+      if (this.filter === 'todo') {
+        return this.todos(todo => !todo.completed)
+      } else if (this.filter === 'done') {
+        return this.todos.filter(todo => todo.completed)
+      }
+      return this.todos
+    }
+  }
+}
+</script>
+
+<style src="./todos.css"></style>
